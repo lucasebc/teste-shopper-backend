96f475f17367758da2faf3c074d40ce32074c380
Lucas Emanuel <8696792+lucasebc@users.noreply.github.com>
2024-08-29 19:44:35 -0300

    adds config to increase request size to 2mb

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
+
+  app.use(bodyParser.json({ limit: '2mb' }));
+  app.use(bodyParser.urlencoded({ limit: '2mb', extended: true }));
+
   await app.listen(3000);
 }
 bootstrap();
