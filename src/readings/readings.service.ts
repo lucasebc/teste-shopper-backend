cf63805081630e923218ab9ae71fc4d6c170c909
Lucas Emanuel <8696792+lucasebc@users.noreply.github.com>
2024-08-29 19:44:11 -0300

    adds gemini code to retrieve data from the image

--- src/readings/readings.service.ts
+++ src/readings/readings.service.ts
@@ -3,8 +3,11 @@ import { randomUUID } from "crypto";
 import { PrismaService } from "src/prisma.service";
 import { UploadReadingDTO } from "./dtos/uploadReading.dto";
 import { ConfirmReadingDTO } from "./dtos/confirmReading.dto";
-import {writeFile, mkdir} from 'fs'
+import {writeFile, mkdir, writeFileSync, mkdirSync} from 'fs'
 import {Measurement} from '@prisma/client'
+import { GoogleAIFileManager } from "@google/generative-ai/server";
+import { GoogleGenerativeAI } from "@google/generative-ai";
+import { dirname, join } from "path";
 
 @Injectable()
 export class ReadingsService {
@@ -39,42 +42,64 @@ export class ReadingsService {
 
     // takes the base64 image data, creates the image file in the appropriate location(root/images/measurements/:client_id/datetime_image).
     // returns image's location.
-    saveImage(base64Image:string, measureDatetime: number, client_id):string {
+    async saveImage(base64Image:string, measureDatetime: number, client_id: string):Promise<string> {
 
-        // Decode the Base64 string
         const binaryData: Buffer = Buffer.from(base64Image.split(',')[1], 'base64');
 
         const fileExtension: string = base64Image.match(/(?<=\/)([^+;]+)(?=[+;])/g)[0]
         const clientDirectory: string = `/images/measurements/${client_id}`
         const fileLocation:string = `${clientDirectory}/${measureDatetime}.${fileExtension}`
 
-        mkdir(clientDirectory, { recursive: true }, (err) => {
-            if (err) {
-                throw new Error('Erro ao criar diretório.');
-            }
-
-            // Save the binary data to a file
-            writeFile(`./${fileLocation}`, binaryData, (error) => {
-            if (error) throw new Error('Erro ao salvar imagem.')
-
-            console.log('arquivo salvo')
-            });
-        });
+        mkdirSync(clientDirectory, { recursive: true });
+        writeFileSync(`./${fileLocation}`, binaryData);
 
         return fileLocation
     }
 
     // calls geminiService sending images location, returns read value. 
-    readImage(): number {
-        return 42
+    async readImage(imageLocation: string): Promise<number> {
+        // instance of the fileManager to send the image.
+        const fileManager: GoogleAIFileManager = new GoogleAIFileManager(process.env.GEMINI_API_KEY);
+
+        // Upload the file and specify a display name.
+        const uploadResponse = await fileManager.uploadFile(join( dirname(require.main.filename), '..', imageLocation), {
+          mimeType: `image/${imageLocation.split('.').pop()}`,
+          displayName: "water/gas register photo",
+        });
+
+        // instance the handler of the AI.
+        const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+
+        const model = genAI.getGenerativeModel({
+          // Choose a Gemini model.
+          model: "gemini-1.5-pro",
+          // model: "gemini-1.5-flash",
+        });
+
+        // Generate content using text and the URI reference for the uploaded file.
+        const result = await model.generateContent([
+            {
+              fileData: {
+                mimeType: uploadResponse.file.mimeType,
+                fileUri: uploadResponse.file.uri
+              }
+            },
+            { text: "This is a water register. How many cubic meters you read on it, ignoring the red numbers? Give me the number only, inside square brackets, please" },
+          ]);
+
+        // Output the generated text to the console
+        console.log(`google says: `,result.response.text())
+
+        const finalResult: number = Number(result.response.text().match(/(?<=\/)([^+\[]+)(?=[\]])/g)[0])
+
+        return finalResult
     }
     
     async upload(uploadData: UploadReadingDTO): Promise<Measurement> {
         try {
-            console.log(uploadData.measure_datetime, new Date(uploadData.measure_datetime))
-            const imgLocation: string = this.saveImage(uploadData.image, new Date(uploadData.measure_datetime).getTime(), uploadData.customer_code)
+            const imgLocation: string = await this.saveImage(uploadData.image, new Date(uploadData.measure_datetime).getTime(), uploadData.customer_code)
 
-            const value: number = this.readImage()
+            const value: number = await this.readImage(imgLocation)
 
             return await this.prisma.measurement.create({data:{
                 client_id: uploadData.customer_code,
